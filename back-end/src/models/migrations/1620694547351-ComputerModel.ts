5f4d77696564a45f984af4f7acbeaeab6ecf2250
Gabriel Baptistucci Garcia <gabrielbgarcia0999@gmail.com>
2021-05-11 18:23:36 -0300

    computer model

---
+++ back-end/src/models/migrations/1620694547351-ComputerModel.ts
@@ -0,0 +1,29 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class ComputerModel1620694547351 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'computers',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+        },
+        {
+          name: 'title',
+          type: 'varchar',
+          length: '100',
+        },
+        {
+          name: 'description',
+          type: 'text',
+        },
+      ],
+    }), true);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('computers', true);
+  }
+}
