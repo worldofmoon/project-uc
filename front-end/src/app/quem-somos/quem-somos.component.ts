773ee46c6c3d32b087f725f3f6111757c6fdb6d6
rogerzsilva <rogerz0610@gmail.com>
2021-06-06 21:22:24 -0300

    alteracao no quem somos

--- front-end/src/app/quem-somos/quem-somos.component.ts
+++ front-end/src/app/quem-somos/quem-somos.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { ModalidadesService } from '../modalidades.service';
-
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-quem-somos',
   templateUrl: './quem-somos.component.html',
@@ -9,11 +10,13 @@ import { ModalidadesService } from '../modalidades.service';
 })
 export class QuemSomosComponent implements OnInit {
   investimentos;
-  constructor(private userService: UserService,
+  safeUrl;
+  constructor(private userService: UserService,private router: Router,private _sanitizer: DomSanitizer,
     private modalidadeService: ModalidadesService) { }
 
   ngOnInit() {
     this.getInvestimentos();
+    this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/8ZLSXAJLuAQ');
   }
 
   getInvestimentos() {
@@ -21,6 +24,11 @@ export class QuemSomosComponent implements OnInit {
     const result = this.modalidadeService.getById(id);
     this.investimentos = result;
     console.log(this.investimentos);
+    
   }
-
+  
+  gotoCadastroClientes() {
+    this.router.navigate(['cadastro-clientes']);
+   }
+   
 }
